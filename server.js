be1e5cb1ad9baf98a0a4a57cd4e607991ba2eeb0
Joel Fintan <joelfintan@gmail.com>
2018-12-07 00:05:08 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+    // res.send('<h1>ASS</h1>');
+    res.render('home.hbs', {
+        pageTitle: 'Home',
+        welcomeMessage: 'Hello World',
+        currentYear: new Date().getFullYear()
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
