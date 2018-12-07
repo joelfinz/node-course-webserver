776a72a412a3ef5fac143c8990a4d997b3fc80fa
Joel Fintan <joelfintan@gmail.com>
2018-12-07 11:09:28 +0530

    Second commit

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const hbs = require('hbs');
 
 var app = express();
 
+const port = process.env.PORT || 3000;
+
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'))
@@ -23,6 +25,6 @@ app.get('/about', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000');
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 });
